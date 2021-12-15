f7b0dfd305cc58a1d90e59c69b2169afab1c4d2f
Angelo <solonasusonly0@gmail.com>
2021-12-15 16:45:25 -0300

    Menu SideBar

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let dayNight = document.querySelector('.dayNight');
+let menuToggle = document.querySelector('.menuToggle');
+let body = document.querySelector('body');
+let nav = document.querySelector('.nav');
+
+dayNight.onclick = function(){
+    body.classList.toggle('dark');
+    dayNight.classList.toggle('active');
+}
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+    body.style.overflow = body.style.overflow == "hidden" ? "visible" : "hidden";
+}
+
+
+
 function preload(){
     document.getElementsByClassName('loading')[0].style.display = "none";
     document.getElementById('webpg').style.display = "block";
