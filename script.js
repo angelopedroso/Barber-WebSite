38d700aef357fc91700b8da6f687eea3aa91671e
Angelo <solonasusonly0@gmail.com>
2021-12-15 20:13:39 -0300

    Dark Mode & Navigation Bar Scrolling

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ let dayNight = document.querySelector('.dayNight');
 let menuToggle = document.querySelector('.menuToggle');
 let body = document.querySelector('body');
 let nav = document.querySelector('.nav');
+let btn = document.querySelector('.btn').getElementsByTagName('ion-icon')[0];
+let header = document.querySelector('.navmenu');
 
 dayNight.onclick = function(){
     body.classList.toggle('dark');
     dayNight.classList.toggle('active');
 }
 
+window.addEventListener('scroll', function(){
+    header.classList.toggle('scrl-color', window.scrollY > (15.5*this.window.innerHeight)/100);
+});
+
 menuToggle.onclick = function(){
     menuToggle.classList.toggle('active');
     nav.classList.toggle('active');
+    header.classList.toggle('dft-color');
     body.style.overflow = body.style.overflow == "hidden" ? "visible" : "hidden";
 }
-
-
 
 function preload(){
     document.getElementsByClassName('loading')[0].style.display = "none";
